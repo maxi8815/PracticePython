246b7f4f5d4e0d5f55c0b79dc9c1f0ecc4cf58a8
Maxi <letona@ar.ibm.com>
2019-07-05 00:25:47 -0300

    Base code - Testing purposes only.

--- Excercise_3.py
+++ Excercise_3.py
@@ -1,25 +1,7 @@
-pos  = -1
 
-
-def search(lista, num):
-    lower = 0
-    upper = len(lista)-1
-
-    while lower <= upper:
-        mid = (lower+upper)//2
-
-        if lista[mid] == num:
-            globals()['pos']=mid
-            return True
-        else:
-            if lista[mid] < num:
-                lower = mid
-            else:
-                upper = mid
-
-
-lista = [15,16,39,41,90]
-num=15
-
-if search(lista,num): print ("found at" ,pos+1)
-else: print("not found")
+open_file = open('python.txt', 'r')
+splitter = open_file.read().split()
+howMuch = 0
+for elements in splitter:
+    howMuch = howMuch +1
+print(howMuch)
